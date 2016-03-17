408ecc194d0ba605bcf941df2ef99c9658d7e958
William S. Moses <taekwonbilly@gmail.com>
2016-03-17 12:38:03 -0400

    added instr

--- src/LLIR.hs
+++ src/LLIR.hs
@@ -1,11 +1,14 @@
 {-# OPTIONS -Wall #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
 
 module LLIR where
 
 -- Imports
 import Prelude
+import Data.List
 import Text.Printf (printf)
 --import ParseTypes
 import qualified Data.Map as HashMap
@@ -17,8 +20,21 @@ data VType            = TInt
                       | TFunction VType [VType]
                       | TVoid
                       | TCallout
+                      | TArray VType
                       | TPointer VType
                       deriving (Eq, Show);
+
+getDerivedType :: VType -> Maybe VType
+getDerivedType a =
+  case a of
+    TPointer ty -> Just ty
+    _ -> Nothing
+
+getArrayType :: VType -> Maybe VType
+getArrayType a =
+  case a of
+    TArray ty -> Just ty
+    _ -> Nothing
 
 class Namable a where
   getName :: a -> String
@@ -27,56 +43,67 @@ class Locationable a where
   setInsertionPoint :: a -> Builder -> Builder
 
 class Value a where
-  getType :: a -> VType
-
-
-data VConstant = VConstInt Int
-               | VConstString String
-  deriving(Eq, Show);
-
-instance Value VConstant where
-  getType (VConstInt _) = TInt
-  getType (VConstString _) = TString
+  getType :: Builder -> a -> VType
 
 data ValueRef = InstRef String
-              | ConstRef VConstant
-              | BlockRef String
+              | ConstInt Int
+              | ConstString String
               | CalloutRef String
               | FunctionRef String
+              | ArgRef Int String
   deriving(Eq, Show);
 
-data VInstruction = VArgument String {- index -} Int VType
-                  | VUnOp {- name -} String {- operand -} String {- argument name -} ValueRef
+instance Value ValueRef where
+  getType builder (InstRef str) =
+    case getInstruction builder str of
+      Just inst -> getType builder inst
+      Nothing -> TVoid
+  getType _ (ConstInt _) = TInt
+  getType _ (ConstString _) = TString
+  getType _ (CalloutRef _) = TCallout
+  getType builder (FunctionRef str) =
+    case getFunction builder str of
+      Just func -> getType builder func
+      Nothing -> TVoid
+  getType builder (ArgRef idx str) =
+    case do
+      func <- getFunction builder str
+      let args :: [VType] = arguments func
+      if idx < length args then Just $ args !! idx else Nothing
+    of
+      Just t -> t
+      Nothing -> TVoid
+
+data VInstruction = VUnOp {- name -} String {- operand -} String {- argument name -} ValueRef
                   | VBinOp {- name -} String {- operand -} String {- argument name -} ValueRef ValueRef
-                  | VAssignment String String VType ValueRef ValueRef
-                  | VLookup String
-                  | VAllocation String
-                  | VArrayLookup String
-                  | VArrayLen String
-                  | VReturn String
-                  | VCondBranch String
-                  | VUncondBranch String
+                  | VStore  String {- tostore -} ValueRef {- location -} ValueRef
+                  | VLookup String ValueRef
+                  | VAllocation String {- type -} VType {- if array -} (Maybe Int)
+                  | VArrayStore String {- tostore -} ValueRef {- array -} ValueRef {- idx -} ValueRef
+                  | VArrayLookup String {- array -} ValueRef {- idx -} ValueRef
+                  | VArrayLen String ValueRef
+                  | VReturn String (Maybe ValueRef)
+                  | VCondBranch String {-cond-} ValueRef {-true-} String {-false-} String
+                  | VUncondBranch String String
   deriving (Eq, Show);
 
 instance Namable VInstruction where
-  getName (VArgument name _ _ ) = name
   getName (VUnOp name _ _ ) = name
   getName (VBinOp name _ _ _ ) = name
-  getName (VAssignment name _ _ _) = name
-  getName (VLookup name) = name
-  getName (VAllocation name) = name
-  getName (VArrayLookup name) = name
-  getName (VArrayLen name) = name
-  getName (VReturn name) = name
-  getName (VCondBranch name) = name
-  getName (VUncondBranch name) = name
+  getName (VStore name _ _) = name
+  getName (VLookup name _) = name
+  getName (VAllocation name _ _) = name
+  getName (VArrayLookup name _ _) = name
+  getName (VArrayLen name _) = name
+  getName (VReturn name _) = name
+  getName (VCondBranch name _ _ _) = name
+  getName (VUncondBranch name _) = name
 
 instance Value VInstruction where
-  getType (VArgument _ _ typ ) = typ
-  getType (VUnOp _ op _ )
+  getType _ (VUnOp _ op _ )
     | op == "-"   =  TInt
     | op == "!"   =  TBool
-  getType (VBinOp _ op _ _)
+  getType _ (VBinOp _ op _ _)
     | op == "+"   =  TInt
     | op == "-"   =  TInt
     | op == "*"   =  TInt
@@ -90,20 +117,30 @@ instance Value VInstruction where
     | op == "!="  =  TBool
     | op == "&"  =  TBool
     | op == "|"  =  TBool
-  getType (VAssignment _ _ typ _ _) = typ
-  -- getType (VLookup) =
-  -- getType (VAllocation) =
-  -- getType (VArrayLookup) =
-  -- getType (VArrayLen) =
-  -- getType (VReturn) =
-  -- getType (VCondBranch) =
-  -- getType (VUncondBranch) =
+  getType _ (VStore _ toStore _) = TVoid
+  getType b (VLookup _ toLookup) =
+    case getDerivedType $ getType b toLookup of
+      Just a -> a
+      Nothing -> TVoid
+  getType _ (VAllocation _ a Nothing) = TPointer a
+  getType _ (VAllocation _ a (Just _) ) = TArray a
+
+  getType _ (VArrayStore _ _ _ _) = TVoid
+  getType b (VArrayLookup _ toLookup _ ) =
+    case getArrayType $ getType b toLookup of
+      Just a -> a
+      Nothing -> TVoid
+
+  getType _ (VArrayLen _ _ ) = TInt
+  getType _ (VReturn _ _ ) = TVoid
+  getType _ (VCondBranch _ _ _ _) = TVoid
+  getType _ (VUncondBranch _ _) = TVoid
 
 data VCallout = VCallout String
   deriving(Eq, Show);
 
 instance Value VCallout where
-  getType _ = TCallout
+  getType _ _ = TCallout
 
 instance Namable VCallout where
   getName (VCallout name) = name
@@ -117,7 +154,7 @@ data VFunction    = VFunction {
 } deriving (Eq, Show);
 
 instance Value VFunction where
-  getType (VFunction _ returnType arguments _ _ ) = TFunction returnType arguments
+  getType _ (VFunction _ returnType arguments _ _ ) = TFunction returnType arguments
 
 instance Namable VFunction where
   getName func = (functionName func)
@@ -162,6 +199,20 @@ data Builder          = Builder {
   location :: Context
 } deriving (Eq, Show);
 
+getFunction :: Builder -> String -> Maybe VFunction
+getFunction (Builder pmod (Context fname _)) name = HashMap.lookup fname (functions pmod)
+
+getInstruction :: Builder -> String -> Maybe VInstruction
+getInstruction (Builder pmod (Context fname _)) name =
+  do
+    func <- HashMap.lookup fname (functions pmod)
+    HashMap.lookup name (functionInstructions func)
+
+instance Locationable (String,String) where
+  setInsertionPoint (str1,str2) builder = setInsertionPoint (Context str1 str2 ) builder
+
+instance Locationable String where
+  setInsertionPoint str builder = setInsertionPoint (Context (contextFunctionName $ location builder) str ) builder
 
 instance Locationable VBlock where
   setInsertionPoint block builder = setInsertionPoint (Context (blockFunctionName block) (blockName block) ) builder
@@ -197,7 +248,7 @@ createUnaryOp op operand (Builder pmod context) =
 
 createBinOp :: String -> ValueRef -> ValueRef -> Builder -> (ValueRef, Builder)
 createBinOp op operand1 operand2 (Builder pmod context) =
-  let (name, pmod2) :: (String, PModule) - createID pmod
+  let (name, pmod2) :: (String, PModule) = createID pmod
       builder2 :: Builder = appendInstruction (VBinOp name op operand1 operand2) (Builder pmod2 context)
       ref :: ValueRef = InstRef name in
       (ref, builder2)
@@ -208,9 +259,9 @@ createBlockF str func =
       block = VBlock (functionName func) str2 []
       in func {blocks = HashMap.insert str2 block $ blocks func }
 
-createBlock :: String -> Builder -> Builder
+createBlock :: String -> Builder -> (String, Builder)
 createBlock str (Builder pmod context) =
-  let updated :: Maybe Builder =
+  let updated :: Maybe (String,Builder) =
         do
           let fn = contextFunctionName context
           func <- HashMap.lookup fn (functions pmod)
@@ -220,18 +271,20 @@ createBlock str (Builder pmod context) =
           let newBlocks = HashMap.insert str2 block oldBlocks
           func2 <- return $ func{blocks=newBlocks}
           functions2 <- return $ HashMap.insert fn func2 (functions pmod)
-          return $ Builder pmod{functions=functions2} context
+          return $ (str2, Builder pmod{functions=functions2} context)
       in case updated of
         Just builder -> builder
-        Nothing -> Builder pmod context
+        Nothing -> ("",Builder pmod context)
+
 
 -- assume no function exists with name currently
 createFunction :: String -> VType -> [VType] -> Builder -> Builder
 createFunction str ty1 argTypes (Builder pmod context) =
   let func = VFunction str ty1 argTypes (HashMap.empty) (HashMap.empty)
       func2 = createBlockF "entry" func
-      instrs = foldl ( \acc (idx, typ) -> HashMap.insert ("$a" ++ (show idx)) (VArgument ("$a" ++ (show idx)) idx typ ) acc) (functionInstructions func) ( zip [0..(length argTypes)] argTypes )
-      func3 = func2{functionInstructions=instrs}
+--      instrs = foldl ( \acc (idx, typ) -> HashMap.insert ("$a" ++ (show idx)) (VArgument ("$a" ++ (show idx)) idx typ ) acc) (functionInstructions func) ( zip [0..(length argTypes)] argTypes )
+--      func3 = func2{functionInstructions=instrs}
+      func3 = func2
       functions2 = HashMap.insert str func3 (functions pmod) in
         Builder pmod{functions=functions2} context
 
