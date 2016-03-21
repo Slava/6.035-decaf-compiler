7912412c70f56117dd6aa5c3b179fdc1efcb2ca8
Bingfei <tonycao96@gmail.com>
2016-03-20 23:41:21 -0400

    use context now

--- src/CodeGen.hs
+++ src/CodeGen.hs
@@ -14,32 +14,37 @@ data CGContext = CGContext {
   nextConstStrId :: Int
 };
 
-getConstStrId :: CGContext -> String -> (CGContext, String)
-getConstStrId cx str =
-  (CGContext {
-      constStrs = (constStrs cx) ++ [(id, str)],
-      nextConstStrId = (nextConstStrId cx) + 1
-   }, id)
-  where id = ".const_str" ++ (show (nextConstStrId cx))
+newCGContext :: CGContext
+newCGContext = CGContext [] 0
+
+getConstStrId :: FxContext -> String -> (FxContext, String)
+getConstStrId (FxContext (CGContext strs next) table offset) str =
+  (FxContext newGlobal table offset, id)
+  where id = ".const_str" ++ (show next)
+        newGlobal = CGContext{
+          constStrs = strs ++ [(id, str)],
+          nextConstStrId = next + 1
+        }
 
 data FxContext = FxContext {
+  global :: CGContext,
   variables :: HashMap.Map String String,
   offset :: Int
 }
 
-newContext :: FxContext
-newContext = FxContext HashMap.empty 8
+newFxContext :: CGContext -> FxContext
+newFxContext global = FxContext global HashMap.empty 8
 
 updateOffset :: FxContext -> FxContext
-updateOffset (FxContext table offset) = FxContext table $ offset + 8
+updateOffset (FxContext global table offset) = FxContext global table $ offset + 8
 
 lookupVariable :: FxContext -> String -> String
-lookupVariable (FxContext table _) var = case HashMap.lookup var table of
+lookupVariable (FxContext _ table _) var = case HashMap.lookup var table of
                                            Just a -> a
                                            Nothing -> "BAD"
 
 setVariableLoc :: FxContext -> String -> String -> FxContext
-setVariableLoc (FxContext table offset) var loc = FxContext (HashMap.adjust update var table) offset
+setVariableLoc (FxContext global table offset) var loc = FxContext global (HashMap.adjust update var table) offset
   where update _ = loc
 
 getHeader :: String
@@ -110,24 +115,24 @@ genCallouts callouts =
 
 genFunction :: CGContext -> LLIR.VFunction -> (CGContext, String)
 genFunction cx f =
-  let prolog = getProlog (8 * (length $ (LLIR.functionInstructions f))) in
-  let (ncx, locals, blocksStr) = foldl
-                   (\(cx, table, s) name ->
+  let prolog = getProlog (8 * (length $ (LLIR.functionInstructions f)))
+      (ncx, blocksStr) = foldl
+                   (\(cx, s) name ->
                      let block = HashMap.lookup name $ LLIR.blocks f
-                         (ncx, hm, str) = genBlock cx block f table in
-                     (ncx, hm, s ++ str))
-                   (cx, HashMap.empty :: HashMap.Map String String, "") $ LLIR.blockOrder f in
-   let strRes = "_" ++ LLIR.functionName f ++ ":\n" ++ prolog ++ blocksStr ++ getEpilog in
-   (cx, strRes)
+                         (ncx, str) = genBlock cx block f in
+                     (ncx, s ++ str))
+                   (newFxContext cx, "") $ LLIR.blockOrder f
+      strRes = "_" ++ LLIR.functionName f ++ ":\n" ++ prolog ++ blocksStr ++ getEpilog in
+    (cx, strRes)
 
-genBlock :: CGContext -> Maybe LLIR.VBlock -> LLIR.VFunction -> HashMap.Map String String -> (CGContext, HashMap.Map String String, String)
-genBlock cx Nothing _ table = (cx, table, "BAD\n")
-genBlock cx (Just block) f table = 
-  foldl (\(cx, t, acc) name ->
+genBlock :: FxContext -> Maybe LLIR.VBlock -> LLIR.VFunction -> (FxContext, String)
+genBlock cx Nothing _ = (cx, "BAD\n")
+genBlock cx (Just block) f = 
+  foldl (\(cx, acc) name ->
           let instruction = HashMap.lookup name $ LLIR.functionInstructions f
-              (ncx, table, str) = genInstruction cx instruction t in
-                (ncx, table, acc ++ str))
-        (cx, table, "")
+              (ncx, str) = genInstruction cx instruction in
+                (ncx, acc ++ str))
+        (cx, "")
         (LLIR.blockInstructions block)
 
 valLoc :: LLIR.ValueRef -> HashMap.Map String String -> String
@@ -157,109 +162,94 @@ valLoc (FunctionRef name) table =
     Just s -> s
     Nothing -> "ERROR"
 
-genInstruction :: CGContext -> Maybe LLIR.VInstruction -> HashMap.Map String String -> (CGContext, HashMap.Map String String, String)
-genInstruction cx Nothing table = (cx, table, "BAD\n")
+genInstruction :: FxContext -> Maybe LLIR.VInstruction -> (FxContext, String)
+genInstruction cx Nothing = (cx, "BAD\n")
 
-genInstruction cx (Just (VAllocation _ typ size)) table =
+genInstruction cx (Just (VAllocation _ typ size)) =
   case size of
-    Just i -> (cx, table, "")
-    Nothing -> (cx, table, "")
+    Just i -> (cx, "")
+    Nothing -> (cx, "")
 
-genInstruction cx (Just (VUnOp _ op val)) table =
-  let loc = valLoc val table
+genInstruction cx (Just (VUnOp _ op val)) =
+  let loc = valLoc val $ variables cx 
       final = case val of
         ConstInt _ -> ""
         _ -> "  movq %rax " ++ loc ++ "\n" in
-    (cx, table, 
+    (cx, 
     "  movq " ++ loc ++ "%rax\n" ++
     "  " ++ op ++ " %rax %rax\n" ++ -- what do I do with this value? I should be creating a new temporary variable and adding a new entry in the table for it
     final)
 
-genInstruction cx (Just (VBinOp _ op val1 val2)) table =
-    let loc1 = valLoc val1 table
-        loc2 = valLoc val2 table in
-          (cx, table, 
+genInstruction cx (Just (VBinOp _ op val1 val2)) =
+    let loc1 = valLoc val1 $ variables cx
+        loc2 = valLoc val2 $ variables cx in
+          (cx,
           "  movq" ++ loc1 ++ "%rax\n" ++
           "  " ++ op ++ loc2 ++ "% rax\n" ++ -- what do I do with this value? I should be creating a new temporary variable and adding a new entry in the table for it
           "TODO")
 
-genInstruction cx (Just (VMethodCall name isName fname args)) table =
+genInstruction cx (Just (VMethodCall name isName fname args)) =
   -- push arguments
-  let (ncx, ntable, nargs) = foldl (\(cx, table, acc) arg ->
-                                      let (ncx, narg) = genArg cx table arg in
-                                        (ncx, table, acc ++ [narg]))
-                                   (cx, table, []) args
+  let (ncx, nargs) = foldl (\(cx, acc) arg ->
+                              let (ncx, narg) = genArg cx arg in
+                                    (ncx, acc ++ [narg]))
+                           (cx, []) args
       precall = getPreCall nargs
       postcall = getPostCall
       destination = (show $ 8 * (-1)) ++ "(%bpx)" in
-        (ncx, HashMap.insert name destination ntable, precall ++ "  call " ++ fname ++ "\n  movq %eax " ++ destination ++ "\n")
+        (setVariableLoc ncx name destination, precall ++ "  call " ++ fname ++ "\n  movq %eax " ++ destination ++ "\n")
 
-genInstruction cx (Just (VStore _ _ _)) table =
-  (cx, table, "TODO")
+genInstruction cx (Just (VStore _ _ _)) =
+  (cx, "TODO")
 
+genInstruction cx (Just (VLookup _ _)) =
+  (cx, "TODO")
 
-genInstruction cx (Just (VLookup _ _)) table =
-  (cx, table, "TODO")
+genInstruction cx (Just (VArrayStore _ _ _ _)) =
+  (cx, "TODO")
 
+genInstruction cx (Just (VArrayLookup _ _ _)) =
+  (cx, "TODO")
 
-genInstruction cx (Just (VArrayStore _ _ _ _)) table =
-  (cx, table, "TODO")
+genInstruction cx (Just (VArrayLen _ _)) =
+  (cx, "TODO")
 
+genInstruction cx (Just (VReturn _ _)) =
+  (cx, "TODO")
 
-genInstruction cx (Just (VArrayLookup _ _ _)) table =
-  (cx, table, "TODO")
+genInstruction cx (Just (VCondBranch _ _ _ _)) =
+  (cx, "TODO")
 
+genInstruction cx (Just (VUnreachable _)) =
+  (cx, "TODO")
 
-genInstruction cx (Just (VArrayLen _ _)) table =
-  (cx, table, "TODO")
+genInstruction cx (Just (VUncondBranch _ _)) =
+  (cx, "TODO")
 
-
-genInstruction cx (Just (VReturn _ _)) table =
-  (cx, table, "TODO")
-
-
-genInstruction cx (Just (VCondBranch _ _ _ _)) table =
-  (cx, table, "TODO")
-
-
-genInstruction cx (Just (VUnreachable _)) table =
-  (cx, table, "TODO")
-
-
-genInstruction cx (Just (VUncondBranch _ _)) table =
-  (cx, table, "TODO")
-
-
-genArg :: CGContext -> HashMap.Map String String -> ValueRef -> (CGContext, (String, Int))
-genArg cx table (InstRef ref) =
+genArg :: FxContext -> ValueRef -> (FxContext, (String, Int))
+genArg cx (InstRef ref) =
   {-- TODO: look up global vars in the CGContext! --}
-  let r = HashMap.lookup ref table in
-  case r of
-    Just addr -> (cx, (addr, 8))
-    Nothing -> (cx, ("BAD", 8))
-
-genArg cx table (ConstInt i) =
+  (cx, (lookupVariable cx ref, 8))
+  
+genArg cx (ConstInt i) =
   (cx, ("$" ++ (show i), 8))
 
-genArg cx table (ConstString s) =
+genArg cx (ConstString s) =
   let (ncx, id) = getConstStrId cx s in
     (ncx, ("$" ++ id, 8))
 
-genArg cx table (ConstBool b) =
+genArg cx (ConstBool b) =
   (cx, ("$" ++ (if b then "1" else "0"), 8))
 
-genArg cx table (ArgRef i funcName) =
-  let r = HashMap.lookup funcName ++ "@" ++ (show i) table in
-  case r of
-    Just addr -> (cx, (addr, 8))
-    Nothing -> (cx, ("BAD", 8))
+genArg cx (ArgRef i funcName) =
+  (cx, (lookupVariable cx $ funcName ++ "@" ++ (show i), 8))
 
 gen :: LLIR.PModule -> String
 gen mod =
   let globals = LLIR.globals mod
       callouts = LLIR.callouts mod
       fxs = HashMap.elems $ LLIR.functions mod
-      cx = CGContext {constStrs = [], nextConstStrId = 0}
+      cx = newCGContext
       (cx2, fns) =
         foldl (\(cx, asm) fn ->
                 let (ncx, str) = genFunction cx fn in
